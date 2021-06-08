c1482d1188bd7568a40fdfbc2186cf72a0a103e3
Al Zahraa Mekky <eng.alzahraa.meky@gmail.com>
2021-06-08 19:35:40 +0400

    update

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Nice Scroll
+    $("body").niceScroll({
+        cursorcolor:"#B134A4",
+        cursorwidth:"8px"
+      });
+  var slider_1 = $('#slider-1').CircularSlider();
+  var slider_2 = $('#slider-2').CircularSlider();
+});
